65214b8734a31e41836ae87ff32f1ef97c7d6808
chiranjitk <chiranjitk@outlook.com>
2026-04-29 10:42:48 +0000

    113ff1b7-fe04-4f2c-8d1b-6d83a74f5251

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,26 +2,26 @@ module.exports = {
   apps: [
     {
       name: 'staysuite-freeradius',
-      script: '/home/z/my-project/StaySuite-HospitalityOS/freeradius-install/sbin/radiusd',
-      args: '-d /home/z/my-project/StaySuite-HospitalityOS/freeradius-install/etc/raddb -D /home/z/my-project/StaySuite-HospitalityOS/freeradius-install/share/freeradius -l /home/z/my-project/StaySuite-HospitalityOS/freeradius-install/var/log/radius.log -f',
-      cwd: '/home/z/my-project/StaySuite-HospitalityOS',
+      script: '/home/z/my-project/freeradius/sbin/radiusd',
+      args: '-d /home/z/my-project/freeradius/etc/raddb -f',
+      cwd: '/home/z/my-project',
       interpreter: 'none',
       watch: false,
       autorestart: true,
       env: {
-        LD_LIBRARY_PATH: '/home/z/my-project/StaySuite-HospitalityOS/freeradius-install/lib',
+        LD_LIBRARY_PATH: '/home/z/my-project/freeradius-deps/usr/local/lib',
       },
     },
     {
       name: 'staysuite-nextjs',
       script: 'bun',
       args: 'run dev',
-      cwd: '/home/z/my-project/StaySuite-HospitalityOS',
+      cwd: '/home/z/my-project',
       watch: false,
       autorestart: true,
       env: {
         PORT: 3000,
-        DATABASE_URL: 'postgresql://z@localhost:5432/staysuite',
+        DATABASE_URL: 'postgresql://postgres:postgres@localhost:5432/staysuite',
         NODE_OPTIONS: '--max-old-space-size=1536',
       },
       max_memory_restart: '2G',
