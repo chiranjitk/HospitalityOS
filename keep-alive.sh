9b74d232b01979a08721fb83ad3a6f1314d7bd1f
Z User <z@container>
2026-05-15 11:50:08 +0000

    a6dd3c44-a228-41b0-8fe9-5fc56b893c56

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd /home/z/my-project
 export DATABASE_URL="postgresql://staysuite:Staysuite2025@127.0.0.1:5432/staysuite"
+export NODE_OPTIONS="--max-old-space-size=4096"
 export NEXT_DISABLE_TURBOPACK=1
 while true; do
-  echo "[$(date)] Starting Next.js dev server..."
-  node_modules/.bin/next dev -p 3000 </dev/null
-  echo "[$(date)] Server exited, restarting in 2s..."
+  node_modules/.bin/next dev -p 3000 -H 0.0.0.0 </dev/null
   sleep 2
 done
